6e0af7cb0c46fc64c4b842072d5eac142aa11a26
Norshy <timorhyprogra@gmail.com>
2026-02-24 21:12:45 +0100

    Small commit.
    I forgot to do a <<git add .>>, so Main.py didn't update.
    Now it does do what I commented in last commit.

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
-# |---- Main imports -------------
+# |---- Main imports   ------------
 import pygame
 
 
-# |---- Own imports  -------------
-from source import display
+# |---- Own imports   ------------
+from source.scripts import display
 
 
-
+# |---- Main code    ------------
+# |------------------------------
+# |---- Starts PyGame -----------
 pygame.init()
+
+# |---- Draws the screen and gives the title of it ------------
+game_screen = pygame.display.set_mode((display.default_width, display.default_height))
+pygame.display.set_caption("Last ashes of The Morning")
+
+ingame_clock = pygame.time.Clock()
+isit_running = True
+
+while isit_running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            isit_running = False
+
+    game_screen.fill(display.default_background)
+
+    # CODE
+
+
+    pygame.display.flip()
+    ingame_clock.tick(30)
+
+pygame.quit()
